e1338773bbe11beb5133a7b60a389fd6b6335169
danieljoppi <daniel.joppi@gmail.com>
2015-07-27 19:25:17 -0300

    parse dates of educations, positions and projects

--- data-containers/education.js
+++ data-containers/education.js
@@ -1,10 +1,19 @@
-function Education(name, link, program, major, gpa, date) {
+function Education(name, link, program, major, gpa, dates) {
 	this.name = name;
 	this.link = link;
 	this.program = program;
 	this.major = major;
 	this.gpa = gpa;
-	this.date = date;
+	this.dates = (function (dates) {
+		var dd = (~dates.indexOf('(') ? dates.substring(0, dates.indexOf('(')) : dates).split('–'),
+			current = dd[1] && ~dd[1].indexOf('Present') ? true : false;
+
+		return {
+			start: dd[0] ? new Date(dd[0]).toJSON() : undefined,
+			end: dd[1] && !current ? new Date(dd[1]).toJSON() : undefined,
+			current: current
+		};
+	})(dates);
 }
 
 module.exports = Education;
--- data-containers/experience.js
+++ data-containers/experience.js
@@ -1,9 +1,19 @@
 function Experience(position, companyName, dates, locality, description) {
-	this.position = position;
-        this.companyName = companyName;
-        this.dates = dates;
-        this.locality = locality;
-        this.description = description;
+    this.position = position;
+    this.companyName = companyName;
+    this.dates = (function (dates) {
+        var dd = (~dates.indexOf('(') ? dates.substring(0, dates.indexOf('(')) : dates).split('–'),
+            current = dd[1] && ~dd[1].indexOf('Present') ? true : false;
+        console.log('>>>', dates,  dd);
+        return {
+            start: dd[0] ? new Date(dd[0]).toJSON() : undefined,
+            end: dd[1] && !current ? new Date(dd[1]).toJSON() : undefined,
+            current: current
+        };
+    })(dates);
+    //this.dates = dates;
+    this.locality = locality;
+    this.description = description;
 }
 
 module.exports = Experience;
