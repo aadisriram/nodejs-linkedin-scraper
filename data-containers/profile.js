96f27d232e216ce5be7067a5633fcf63982dbe8c
danieljoppi <daniel.joppi@gmail.com>
2015-07-27 18:15:09 -0300

    update scraper: get picture, skills and languages

--- data-containers/profile.js
+++ data-containers/profile.js
@@ -1,16 +1,20 @@
 //constructor
 function LinkedInProfile() {
-	this.name;
-        this.headline;
-        this.location;
-        this.current;
-        this.past = [];
-        this.education;
-        this.websites = [];
-        this.experience = [];
-        this.honors = [];
-        this.projects = [];
-        this.educationlist = [];
+    this.name;
+    this.pictureUrl;
+    this.headline;
+    this.location;
+    this.sumary;
+    this.current;
+    this.past = [];
+    this.education;
+    this.websites = [];
+    this.positions = [];
+    this.honors = [];
+    this.projects = [];
+    this.educations = [];
+    this.skills = [];
+    this.languages = [];
 }
 
 module.exports = LinkedInProfile;
